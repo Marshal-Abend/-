79d0fbb43a25ca460893e25c91d818d55fb82916
Valera Volkov <lone_marshal@mail.ru>
2017-04-28 00:07:27 +0300

    luchshii wide factorial

--- Исследование/main.cpp
+++ Исследование/main.cpp
@@ -59,7 +59,7 @@ string multiply_string(string a, string b) {
 }
 
 
-string factorial(int n) {
+/*string factorial(int n) {
 	if (n<0)
 	{
 		return "";
@@ -77,16 +77,46 @@ string factorial(int n) {
 	}
 	a = reverse_string(a);
 	return a.substr(0,a.length());
+}*/
+
+
+#include<string>
+using namespace std;
+string factorial(int factorial) {
+	int n = factorial;
+	if (n < 0) return "";
+	if (n <= 1)  return "1";
+	std::vector<int> v = { 2 };
+	for (int i = 3; i <= n; i++) {
+		int r = 0; //remainder
+		for (int j = 0; j < (int)v.size(); j++) {
+			v[j] *= i;
+			v[j] += r;
+			r = v[j] / 10;
+			v[j] %= 10;
+		}
+		while (r > 0) {
+			v.push_back(r % 10);
+			r /= 10;
+		}
+	}
+	string result;
+	for (auto i = v.rbegin(); i != v.rend(); i++) {
+		result += '0' + (*i);
+	}
+	return result;
 }
-
-
-
 
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	cout << factorial(1000) << endl;
-
+	int in;
+	cout << "Введите число n! ";
+	cin >> in;
+	ofstream potok("factorial.txt", ios_base::trunc | ios_base::out);
+	potok << factorial(in) << endl;
+	potok.close();
+	cout << "Данные успешно записаны в файл factorial.txt\n";
 	system("pause");
 	return 0;
 }
